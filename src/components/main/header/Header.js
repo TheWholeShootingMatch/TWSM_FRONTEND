53fe96cdc0ddd04caac89c7d475f40ed3ca501e3
Sa Eun Soo <tkdmstn0304@naver.com>
2021-01-05 13:33:59 +0900

    main header (common)

---
+++ src/components/main/header/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Header() {
+    return(
+        <header>
+            <div class="logo"></div>
+            <nav class="header_nav">
+                <ul>
+                    <li>model</li>
+                    <li>photographer</li>
+                    <li>collaborate</li>
+                    <li>mypage</li>
+                    <li>sign in</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header;
