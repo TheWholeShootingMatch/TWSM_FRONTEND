0cc8135b49b87953752f4ab4d770cca4801e19f7
Jiseon-Lee <l990831@naver.com>
2021-01-09 13:22:43 +0900

    login

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,6 +1,40 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import axios from "axios";
 
 function Login(){
+
+  const [inputs, setInputs] = useState({
+    id: '',
+    password: ''
+  });
+
+  const onChange = (e) => {
+    const { value, name } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value
+    });
+  };
+
+  useEffect(() => {
+    const response = async() => {
+      const reslut = await axios({
+        method: 'get',
+        withCredentials : true,
+        url : '/api/login'
+      });
+      console.log('login page');
+    };
+    response();
+  },[]);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    axios.post('/api/login', inputs, {
+      withCredentials: true,
+    });
+    console.log('login page');
+  };
 
     return(
       <main>
@@ -8,9 +42,9 @@ function Login(){
           <img src="" alt="logo"/>
         </div>
         <div className="container">
-          <form>
-            <input className="id_area" type="text" placeholder="ID" name="id" required/>
-            <input className="pw_area" type="password" placeholder="PASSWORD" name="password" required/>
+          <form onSubmit={onSubmit}>
+            <input className="id_area" type="text" placeholder="ID" name="id" onChange={onChange} required/>
+            <input className="pw_area" type="password" placeholder="PASSWORD" name="password" onChange={onChange} required/>
             <a href="#" className="pw_search">Forgot your password?</a>
             <button className="login_btn" type="submit">LOGIN</button>
           </form>
--- src/components/signup/signup.js
+++ src/components/signup/signup.js
@@ -1,26 +1,70 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import axios from "axios";
 
 function Signup(){
 
+  const [inputs, setInputs] = useState({
+    name: "",
+    id: '',
+    email: '',
+    password: '',
+    password_repeat: ''
+  });
+
+  const onChange = (e) => {
+    const { value, name } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value
+    });
+  };
+
+  useEffect(() => {
+    const response = async() => {
+      const reslut = await axios({
+        method: 'get',
+        withCredentials : true,
+        url : '/api/signup'
+      });
+      console.log('signup page');
+    };
+    response();
+  },[]);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    axios.post('/api/signup', inputs, {
+      withCredentials: true,
+    });
+    console.log('signup page');
+  };
+
     return(
-      <form>
-        <label for="name"><b>Name</b></label>
-        <input type="text" placeholder="Enter Name" name="name" required/>
+      <main>
+        <div className="logo">
+          <img src="" alt="logo"/>
+        </div>
+        <div className="container">
+        <form onSubmit={onSubmit}>
+          <label for="name"><b>Name</b></label>
+          <input type="text" name="name" onChange={onChange} required/>
 
-        <label for="id"><b>ID</b></label>
-        <input type="text" placeholder="Enter ID" name="id" required/>
+          <label for="id"><b>ID</b></label>
+          <input type="text" name="id" onChange={onChange} required/>
 
-        <label for="email"><b>E-mail address</b></label>
-        <input type="text" placeholder="Enter E-mail" name="email" required/>
+          <label for="email"><b>E-mail address</b></label>
+          <input type="text" name="email" onChange={onChange} required/>
 
-        <label for="password"><b>Password</b></label>
-        <input type="password" placeholder="Enter Password" name="password" required/>
+          <label for="password"><b>Password</b></label>
+          <input type="password" name="password" onChange={onChange} required/>
 
-        <label for="password_repeat"><b>Repeat Password</b></label>
-        <input type="password" placeholder="Repeat Password" name="password_repeat" required/>
+          <label for="password_repeat"><b>Repeat Password</b></label>
+          <input type="password" name="password_repeat" onChange={onChange} required/>
 
-        <input type="submit" value="SIGN UP"/>
-      </form>
+          <input type="submit" value="SIGN UP"/>
+        </form>
+        </div>
+      </main>
     )
 }
 
