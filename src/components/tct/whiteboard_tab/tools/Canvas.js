796ae9d28ba9b6ee991e491c03f538b0d7d09f76
Sa Eun Soo <tkdmstn0304@naver.com>
2021-01-12 14:24:08 +0900

    collaborate drawing

--- src/components/tct/whiteboard_tab/tools/Canvas.js
+++ src/components/tct/whiteboard_tab/tools/Canvas.js
@@ -1,10 +1,13 @@
 import React, {useEffect, useRef, useState} from "react";
+import * as shared from './SharedTypes';
+import * as Y from 'yjs'
 
 function Canvas({toolType}){
 
     const canvasRef = useRef(null);
     const contextRef = useRef(null);
-    const [isDrawing, setIsDrawing] = useState(false);
+    const [isDrawing, setIsDrawing] = useState(true);
+    let sharedLine = null;
 
     useEffect(() => {
         const canvas = canvasRef.current;
@@ -12,43 +15,99 @@ function Canvas({toolType}){
         canvas.height = 283 * 2;
 
         const context = canvas.getContext("2d");
-        context.lineJoin = context.lineCap = "round";
+        context.lineCap = context.lineJoin = "round";
         context.strokeStyle = "black";
-        context.shadowBlur = 2
-        context.lineWidth = 2;
+        context.lineWidth = "1";
         contextRef.current = context;
     },[])
 
-    const startDrawing = ({nativeEvent}) => {
+    const calculateCoordinate = (event) => {
+        const canvasRect = canvasRef.current.getBoundingClientRect();
+        return {
+            x: (event.clientX - canvasRect.left) / canvasRect.width,
+            y: (event.clientY - canvasRect.top) / canvasRect.height
+        }
+    }
+      
+    const startDrawing = (event) => {
         if(toolType !== "drawing"){
+            // setIsDrawing(false);
             return;
         }
-        const {offsetX, offsetY} = nativeEvent;
-        contextRef.current.beginPath();
-        contextRef.current.moveTo(offsetX, offsetY);
-        setIsDrawing(true);
+        const drawElement = new Y.Map();
+        const coordinate = calculateCoordinate(event);
+        drawElement.set('color','black');
+        drawElement.set('type','path');
+        drawElement.set('coordinate', coordinate);
+        sharedLine = new Y.Array();
+        drawElement.set('path', sharedLine);
+        shared.drawingContent.push([drawElement]);
     }
 
-    const draw = ({nativeEvent}) => {
-        if(!isDrawing){
-            return;
+    const moveDraw = (event) => {
+        if(isDrawing){
+            if(sharedLine !== null){
+                const coordinate = calculateCoordinate(event);
+                sharedLine.push([coordinate]);
+            }
         }
-        const {offsetX, offsetY} = nativeEvent;
-        contextRef.current.lineTo(offsetX, offsetY);
-        contextRef.current.stroke();
     }
 
     const finishDrawing = () => {
-        contextRef.current.closePath();
-        setIsDrawing(false);
+        sharedLine= null;
     }
+
+    shared.drawingContent.observe(function(event) {
+        const context = canvasRef.current.getContext("2d");
+        const yDrawingContent = shared.drawingContent;
+        const requestAnimationFrame = window.requestAnimationFrame || setTimeout;
+
+        const draw = () => {
+            
+            context.clearRect(0,0, context.canvas.width, context.canvas.height);
+            const width = context.canvas.width;
+            const height = context.canvas.height;
+
+            yDrawingContent.forEach(drawElement => {
+                if (drawElement.get('type') === 'path') {
+                    const coordinate =drawElement.get('coordinate');
+                    //   const color = drawElement.get('color');
+                    const path = drawElement.get('path');
+    
+                    if(path){
+                        context.beginPath();
+                        context.moveTo(coordinate.x * width, coordinate.y * height);
+                        let lastPoint = coordinate;
+                        path.forEach(c => {
+                            const pointBetween = {
+                                x : (c.x + lastPoint.x)/2,
+                                y : (c.y + lastPoint.y)/2
+                            }
+                            context.quadraticCurveTo(lastPoint.x * width, lastPoint.y * height, pointBetween.x * width, pointBetween.y * height);
+                            lastPoint = c;
+                        })
+                        context.lineTo(lastPoint.x * width, lastPoint.y * height);
+                        context.stroke();
+                    }
+                }
+            });            
+        }
+        
+        const requestDrawAnimationFrame = () => {
+            requestAnimationFrame(draw);
+        }
+
+        yDrawingContent.observeDeep(requestDrawAnimationFrame);
+        // sharedLine.unregisterYDraw = () => yDrawingContent.unobserveDeep(requestDrawAnimationFrame);
+        requestDrawAnimationFrame();
+    });
 
     return(
         <canvas ref={canvasRef}
         className="current_canvas"
         onMouseDown={startDrawing}
         onMouseUp={finishDrawing}
-        onMouseMove={draw}
+        onMouseMove={moveDraw}
         width="566px" height="283px"/>
     )
 }
