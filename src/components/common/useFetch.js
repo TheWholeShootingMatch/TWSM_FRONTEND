ac0b074db02847b9c702a96eb6e7ef71f80e6f79
yelim park <pyl9983@naver.com>
2021-01-16 23:48:46 +0900

    update comment

--- src/components/common/useFetch.js
+++ src/components/common/useFetch.js
@@ -1,10 +1,10 @@
 import React, {useEffect, useState} from "react";
 
-function useFetch(url) {
+function useFetch(url, param) {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
   async function fetchUrl() {
-    const response = await fetch(url);
+    const response = await fetch(url, param);
     const json = await response.json();
     setData(json);
     setLoading(false);
