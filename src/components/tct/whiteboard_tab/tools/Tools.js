e55ef814afa352dbe1ed1f7a5e6ba79f98729c67
Sa Eun Soo <tkdmstn0304@naver.com>
2021-01-19 14:47:07 +0900

    add tool actions

---
+++ src/components/tct/whiteboard_tab/tools/Tools.js
@@ -0,0 +1,21 @@
+import * as shared from './SharedTypes';
+
+/* delete all (trash action) */
+export const deleteAllDrawing = () => {
+    shared.drawingContent.delete(0, shared.drawingContent.length);
+} 
+
+/* undo drawing (undo action) */
+export const undoDrawing = () => {
+    shared.whiteboardUndoManager.undo();
+}
+
+/* redo drawing (redo action) */
+export const redoDrawing = () => {
+    shared.whiteboardUndoManager.redo();
+}
+
+export const showVersionList = () => {
+    const versions = shared.getListVersion();
+    return versions;
+}
