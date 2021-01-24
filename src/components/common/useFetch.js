a86659d27e576800fce4d22b7b6f204fa9bd1407
yelim park <pyl9983@naver.com>
2021-01-24 16:43:44 +0900

    fix server error

--- src/components/common/useFetch.js
+++ src/components/common/useFetch.js
@@ -3,8 +3,13 @@ import React, {useEffect, useState} from "react";
 function useFetch(url, param) {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
+  const option = Object.assign({
+    headers : {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    }}, param);
   async function fetchUrl() {
-    const response = await fetch(url, param);
+    const response = await fetch(url, option);
     const json = await response.json();
     setData(json);
     setLoading(false);
