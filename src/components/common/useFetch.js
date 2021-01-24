c318540a04fd9d7faef1e6828c037117586d8508
yelim park <pyl9983@naver.com>
2021-01-24 16:47:36 +0900

    fix server error

--- src/components/common/useFetch.js
+++ src/components/common/useFetch.js
@@ -8,6 +8,7 @@ function useFetch(url, param) {
       'Content-Type': 'application/json',
       'Accept': 'application/json'
     }}, param);
+  console.log(option);
   async function fetchUrl() {
     const response = await fetch(url, option);
     const json = await response.json();
