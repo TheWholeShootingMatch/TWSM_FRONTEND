b340551c2be1871c2add72640aec878a343d0edb
Sa Eun Soo <tkdmstn0304@naver.com>
2021-01-26 14:11:24 +0900

    header contents change wether user login status

--- src/components/main/header/Header.js
+++ src/components/main/header/Header.js
@@ -1,16 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./Header.scss";
 
-function Header() {
+function Header({ isLogin }) {
+
+    /* 유저가 관리자냐 일반 유저냐에 따라 링크 조정 필요 */
+
     return(
-        <header>
+        <header className="main_header">
             <div className="logo"></div>
             <nav className="header_nav">
                 <ul>
                     <li>model</li>
                     <li>photographer</li>
                     <li>collaborate</li>
-                    <li>mypage</li>
-                    <li>sign in</li>
+                    <li>{isLogin ? <Link to="/mypage/user">my page</Link> : <Link to="/login">my page</Link>}</li>
+                    <li>{isLogin ? <Link to="/logout">logout</Link> : <Link to="/login">login</Link>}</li> 
                 </ul>
             </nav>
         </header>
