d86e7bb630129b9b1aab1eade8e02f7f2ca02e52
Sa Eun Soo <tkdmstn0304@naver.com>
2021-01-26 14:13:12 +0900

    add main header in overview page

--- src/components/mypage/common/Overview.js
+++ src/components/mypage/common/Overview.js
@@ -1,9 +1,14 @@
 import React from "react";
+import { Redirect } from 'react-router-dom';
 import MyPage from "./MyPage";
 
-function Overview({type}){
+function Overview({ type, isLogin }) {
+    if (!isLogin) {
+        alert("로그인 후 이용하세요!");
+        return <Redirect to={{pathname: "/login"}}/>
+    }
     return(
-        <MyPage user={type} header="Overview">
+        <MyPage user={type} header="Overview" isLogin={isLogin}>
             <OverviewPropjects user={type}/>
         </MyPage>
     )
