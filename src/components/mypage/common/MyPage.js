4acfcc8dc2b41831be5e0d6188b326aa61cf87d7
Sa Eun Soo <tkdmstn0304@naver.com>
2021-01-26 14:12:38 +0900

    add main header in mypage

--- src/components/mypage/common/MyPage.js
+++ src/components/mypage/common/MyPage.js
@@ -1,12 +1,18 @@
 import React from "react";
 import {MHeader} from "../Header";
-import {UHeader} from "../Header";
+import { UHeader } from "../Header";
+import Header from "../../main/header/Header";
+import "./MyPage.scss";
 
-function MyPage({children, user, header}){
+function MyPage({ children, user, header, isLogin }) {
+    
+    console.log(isLogin);
 
     if(user === "manager"){
-        return(
-            <div>
+        return (
+        <div>
+            <Header isLogin={isLogin}/>
+            <div className="mypage_wrapper">
                 <MHeader/>
                 <main>
                     <h2 className="mypage_header">{header}</h2>
@@ -15,18 +21,22 @@ function MyPage({children, user, header}){
                     </div>
                 </main>
             </div>
+        </div>
         )
     }
     else{
-        return(
+        return (
             <div>
-                <UHeader/>
-                <main>
-                    <h2 className="mypage_header">{header}</h2>
-                    <div className="mypage_contents">
-                        {children}
-                    </div>
-                </main>
+                <Header isLogin={isLogin}/>
+                <div className="mypage_wrapper">
+                    <UHeader/>
+                    <main>
+                        <h2 className="mypage_header">{header}</h2>
+                        <div className="mypage_contents">
+                            {children}
+                        </div>
+                    </main>
+                </div>
             </div>
         )
     }
