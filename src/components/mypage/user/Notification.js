142062b7c16d966186edbe1b638ed156607e034f
Sa Eun Soo <tkdmstn0304@naver.com>
2021-01-29 22:49:34 +0900

    get notification

--- src/components/mypage/user/Notification.js
+++ src/components/mypage/user/Notification.js
@@ -1,26 +1,39 @@
-import React from "react";
+import React, { useEffect,useState } from "react";
 import UserMyPage from "../common/MyPage";
+import { useFetch } from "../../common/useFetch";
 
 function Notification(){
+    
+    const [notifications] = useFetch('/api/notification');
+    console.log(notifications);
+
     return(
         <UserMyPage user="user" header="Notification">
-            <NotificationTable/>
+            <tbody className="notifications">
+                {notifications.map((notification, index) => <NotificationTable key={index} notification={notification}/>)}
+            </tbody>
         </UserMyPage>
     )
 }
 
-function NotificationTable(){
-    return(
-        <tbody className="notifications">
-            <tr className="message">
-                <td className="check_msg">true</td>
-                <td className="name">kim</td>
-                <td className="title">msg title</td>
-                <td className="date">2020/11/11</td>
-            </tr>
-            <tr className="message"></tr>
-            <tr className="message"></tr>
-        </tbody>
+function NotificationTable({ notification }) {
+                
+    const { TcTnum, sender, sendTime, type, status } = notification;
+    const [msgTitle, setTitle] = useState("");
+
+    useEffect(() => {
+        if (type === "A") {
+            setTitle(`${TcTnum} 프로젝트가 승인되었습니다.`);
+        }
+    },[])
+
+    return (
+        <tr className="message">
+            <td className="msg_checked">{status? "안 읽음" : "읽음"}</td>
+            <td className="sender">{sender}</td>
+            <td className="title">{msgTitle}</td>
+            <td className="date">{new Date(Number(sendTime)).toLocaleDateString()}</td>
+        </tr>
     )
 }
 
