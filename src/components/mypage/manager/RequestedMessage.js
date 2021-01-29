1254d6844f2e4fa3d5c10555bcfc966309acda96
Sa Eun Soo <tkdmstn0304@naver.com>
2021-01-29 22:49:21 +0900

    approve project function

--- src/components/mypage/manager/RequestedMessage.js
+++ src/components/mypage/manager/RequestedMessage.js
@@ -1,37 +1,53 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import ManagerPage from "../common/MyPage";
 import { useFetch } from "../../common/useFetch";
+import axios from "axios";
 
 function RequestedMessage() {
-    
     const [requestedMessage] = useFetch('/api/tct');
-    console.log(requestedMessage);
 
     return(
         <ManagerPage user="manager" header="Requested project">
-            {requestedMessage.map((message, index) => <RequestedProject key={index} message={message}/>)}
+            <tbody className="requested_projects">
+                {requestedMessage.map((message, index) => <RequestedProject key={index} message={message}/>)}
+            </tbody>
         </ManagerPage>
     )
 }
 
 function RequestedProject({ message }) {
     
-    const { owner, description, request_time, status, title } = message;
+    const { _id, owner, description, request_time, status, title } = message;
+    const [approveToggleBtn, setToggleBtn] = useState(false);
+    const [newStatus, setStatus] = useState(status);
+
+    useEffect(() => {
+        if (newStatus === "A") {
+            setToggleBtn(true);
+        }
+    }, []);
+
+    const onApprove = () => {
+        axios.post('/api/tct/approve', { _id, owner, title }, { withCredentials: true })
+            .then(res => {
+                if (res.data) {
+                    setStatus('A');
+                    setToggleBtn(true);
+                }
+            })
+            .catch(err => { alert("fail to approve project!") });
+    }
     
     return(
-        <>
-        <tbody className="requested_projects">
             <tr className="project_info">
                 <td className="username">{owner}</td>
                 <td className="title">{title}</td>
                 <td className="description">{description}</td>
                 <td className="date">{new Date(Number(request_time)).toLocaleDateString()}</td>
-                <td className="status">{status}</td>
-                <td className="approve_btn">approve btn</td>
+                <td className="status">{newStatus}</td>
+                <td className="approve_btn"><button onClick={() => onApprove()} disabled={approveToggleBtn}>approve btn</button></td>
                 <td className="deny_btn">deny btn</td>
             </tr>
-        </tbody>
-        </>
     )
 }
 
