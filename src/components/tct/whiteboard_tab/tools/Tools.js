a0c45c654fef8480d5330f3d2242c5f53d5a2bf3
Sa Eun Soo <tkdmstn0304@naver.com>
2021-03-01 04:15:28 +0900

    image upload tools

--- src/components/tct/whiteboard_tab/tools/Tools.js
+++ src/components/tct/whiteboard_tab/tools/Tools.js
@@ -19,3 +19,16 @@ export const getVersionList = () => {
     const versions = shared.getVersionList();
     return versions;
 }
+
+export const uploadImage = (fileUploaded, context) => {
+    let reader = new FileReader();
+    reader.readAsDataURL(fileUploaded);
+    reader.onload = function (e) {
+        let img = new Image();
+        img.src = e.target.result;
+        console.log(img);
+        img.onload = function () {
+            context.drawImage(img, 0, 0);
+        }
+    }
+}
