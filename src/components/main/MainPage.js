29f22bf5bfe61e9249b8f5bcd26ea3dd5ce226e4
yelim park <pyl9983@naver.com>
2021-03-20 13:08:17 +0900

    search

--- src/components/main/MainPage.js
+++ src/components/main/MainPage.js
@@ -1,11 +1,13 @@
 import React, { useEffect } from "react";
 import Header from "../common/header";
+import Search from "../common/search";
 
 function MainPage({ isLogin }) {
     console.log(isLogin);
     return(
         <div>
             <Header isLogin={isLogin}/>
+            <Search />
             <p>main page</p>
         </div>
     )
