39ec01570a04b58665dc183fae3296cdf37451b7
Sa Eun Soo <tkdmstn0304@naver.com>
2021-04-14 12:57:20 +0900

    make login storage

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,108 +1,131 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from 'react';
 import { Redirect } from 'react-router-dom';
-import axios from "axios";
+import axios from 'axios';
 
-export function Logout({setIsLogin, setUserType}) {
-  useEffect(() => {
-    const response = async() => {
-      await axios({
-        method: 'get',
-        withCredentials : true,
-        url : '/api/users/logout'
-      }).then((res) => {
-        if (res.data === true) {
-          setIsLogin(false);
-          setUserType();
-        }
-      });
-    };
-    response();
-  }, []);
-  alert("logout");
-  return <Redirect to={{pathname: "/"}}/>
+export function Logout({ setIsLogin, setUserType }) {
+    useEffect(() => {
+        const response = async () => {
+            await axios({
+                method: 'get',
+                withCredentials: true,
+                url: '/api/users/logout',
+            }).then((res) => {
+                if (res.data === true) {
+                    setIsLogin(false);
+                    setUserType();
+                    window.localStorage.clear();
+                }
+            });
+        };
+        response();
+    }, []);
+    alert('logout');
+    return <Redirect to={{ pathname: '/' }} />;
 }
 
-export function Login({setUserType, setIsLogin, isLogin}){
-
-  const [inputs, setInputs] = useState({
-    id: '',
-    password: ''
-  });
-
-  // const [isLogin, setIsLogin] = useState(false); //true -> redirect to mainpage   //더 좋은 방법은 모르겠엄..
-
-  const onChange = (e) => {
-    const { value, name } = e.target;
-    setInputs({
-      ...inputs,
-      [name]: value
+export function Login({ setUserType, setIsLogin, isLogin }) {
+    const [inputs, setInputs] = useState({
+        id: '',
+        password: '',
     });
-  };
 
-  useEffect(() => {
-    const response = async() => {
-      await axios({
-        method: 'get',
-        withCredentials : true,
-        url : '/api/users/login'
-      }).then((res) => {
-        if(res.data === true) { //login 기록이 있을 시 redirect("/")
-          alert('already logined');
-          setIsLogin(true);
-        }
-        else {
-          setIsLogin(false);
-        }
-      });
+    // const [isLogin, setIsLogin] = useState(false); //true -> redirect to mainpage   //더 좋은 방법은 모르겠엄..
+
+    const onChange = (e) => {
+        const { value, name } = e.target;
+        setInputs({
+            ...inputs,
+            [name]: value,
+        });
     };
-    response();
-  },[]);
 
-  const onSubmit = (e) => {
-    e.preventDefault();
-    axios.post('/api/users/login', inputs, {
-      withCredentials: true,
-    }).then(res => {
-      console.log(res.data);
-      if(res.data.id === '') {  //login 실패시
-        alert("login fail");
-        setIsLogin(false);
-      }
-      else {  //login 성공
-        if (res.data.id === "manager") {
-          setUserType("manager");
-        }
-        else {
-          setUserType("general");
-        }
-        setIsLogin(true);
-      }
-    });
-  };
+    useEffect(() => {
+        const response = async () => {
+            await axios({
+                method: 'get',
+                withCredentials: true,
+                url: '/api/users/login',
+            }).then((res) => {
+                if (res.data) {
+                    //login 기록이 있을 시 redirect("/")
+                    console.log(res.data.id);
+                    window.localStorage.setItem('id', res.data);
+                    alert('already logined');
+                    setIsLogin(true);
+                } else {
+                    setIsLogin(false);
+                }
+            });
+        };
+        response();
+    }, []);
 
-  if(isLogin) {
-      return (
-        <Redirect to={{
-            pathname: "/"
-          }}/>
-      );
-    }
-    else {
-      return(
-        <main>
-          <div className="logo">
-            <img src="" alt="logo"/>
-          </div>
-          <div className="container">
-            <form onSubmit={onSubmit}>
-              <input className="id_area" type="text" placeholder="ID" name="id" onChange={onChange} required/>
-              <input className="pw_area" type="password" placeholder="PASSWORD" name="password" onChange={onChange} required/>
-              <a href="#" className="pw_search">Forgot your password?</a>
-              <button className="login_btn" type="submit">LOGIN</button>
-            </form>
-            <a href="/signup">SIGN UP</a>
-          </div>
-        </main>
-      );
+    const onSubmit = (e) => {
+        e.preventDefault();
+        axios
+            .post('/api/users/login', inputs, {
+                withCredentials: true,
+            })
+            .then((res) => {
+                if (res.data.id === '') {
+                    //login 실패시
+                    alert('login fail');
+                    setIsLogin(false);
+                } else {
+                    //login 성공
+                    window.localStorage.setItem('id', res.data.id);
+                    if (res.data.id === 'manager') {
+                        setUserType('manager');
+                    } else {
+                        setUserType('general');
+                    }
+                    setIsLogin(true);
+                }
+            });
+    };
+
+    if (isLogin) {
+        return (
+            <Redirect
+                to={{
+                    pathname: '/',
+                }}
+            />
+        );
+    } else {
+        return (
+            <main>
+                <div className="logo">
+                    <img src="" alt="logo" />
+                </div>
+                <div className="container">
+                    <form onSubmit={onSubmit}>
+                        <input
+                            className="id_area"
+                            type="text"
+                            placeholder="ID"
+                            name="id"
+                            onChange={onChange}
+                            required
+                        />
+                        <input
+                            className="pw_area"
+                            type="password"
+                            placeholder="PASSWORD"
+                            name="password"
+                            onChange={onChange}
+                            required
+                        />
+                        <a href="#" className="pw_search">
+                            Forgot your password?
+                        </a>
+                        <button className="login_btn" type="submit">
+                            LOGIN
+                        </button>
+                    </form>
+                    <a href="/signup">SIGN UP</a>
+                </div>
+            </main>
+        );
     }
 }
