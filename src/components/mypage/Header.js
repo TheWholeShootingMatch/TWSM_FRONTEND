0db19b0e59799dd0b641425a1366c86fb9bdf62c
Sa Eun Soo <tkdmstn0304@naver.com>
2021-04-29 10:29:32 +0900

    block user function

--- src/components/mypage/Header.js
+++ src/components/mypage/Header.js
@@ -1,32 +1,49 @@
 import React from "react";
-import {NavLink} from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 /* 일반 유저 */
-export function UHeader(){
-    return(
+export function UHeader() {
+    return (
         <div className="profile_nav">
             <div className="profile_upper">이미지랑 이름</div>
             <ul>
-                <li><NavLink to="/mypage/requested-project">Requested Project</NavLink></li>
-                <li><NavLink to="/mypage/notification">Notification</NavLink></li>
-                <li><NavLink to="/mypage/project">My Project</NavLink></li>
-                <li><NavLink to="/mypage/bookmark">Bookmark</NavLink></li>
-                <li><NavLink to="/mypage/profile">My Profile</NavLink></li>
+                <li>
+                    <NavLink to="/mypage/requested-project">
+                        Requested Project
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to="/mypage/notification">Notification</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/mypage/project">My Project</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/mypage/bookmark">Bookmark</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/mypage/profile">My Profile</NavLink>
+                </li>
             </ul>
         </div>
-    )
+    );
 }
 
 /* 매니저 */
-export function MHeader(){
-    return(
+export function MHeader() {
+    return (
         <div className="profile_nav">
             <div className="profile_upper">이미지랑 이름</div>
             <ul>
-                <li><NavLink to="/mypage/requested-message">requested project</NavLink></li>
-                <li><NavLink to="/manager-page/deleted-project">deleted project</NavLink></li>
-                <li><NavLink to="/manager-page/blocked-user">blocked user</NavLink></li>
+                <li>
+                    <NavLink to="/mypage/requested-message">
+                        requested project
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to="/mypage/blocked-user">blocked user</NavLink>
+                </li>
             </ul>
         </div>
-    )
+    );
 }
--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, Fragment } from "react";
 import axios from "axios";
-import { BrowserRouter, Route, Switch, Router} from "react-router-dom";
+import { BrowserRouter, Route, Switch, Router } from "react-router-dom";
 
 import Header from "../components/common/header";
 import MainPage from "../components/main/MainPage";
@@ -19,7 +19,7 @@ import New_Photographer from "../components/photographer/new_photographer";
 import TctWorkflow from "../components/tct/workflow_tab/TctWorkflow";
 import WhiteBoard from "../components/tct/whiteboard_tab/Whiteboard";
 import TctModel from "../components/tct/model_tab/tct_model";
-import TctPhotographer from "../components/tct/photographer_tab/tct_photographer"
+import TctPhotographer from "../components/tct/photographer_tab/tct_photographer";
 
 /* my page (common) */
 import Overview from "../components/mypage/common/Overview";
@@ -34,6 +34,7 @@ import BookMark from "../components/mypage/user/BookMark";
 
 /* my page (manager) */
 import RequestedMessage from "../components/mypage/manager/RequestedMessage";
+import BlockedUser from "../components/mypage/manager/BlockedUser";
 
 /* signup, login, logout */
 import Signup from "../components/signup/signup";
@@ -45,7 +46,6 @@ import NewCollaborate from "../components/collaborate/NewCollaborate";
 import CollaborateDetail from "../components/collaborate/CollaborateDetail";
 
 function Routes() {
-
     const [isLogin, setIsLogin] = useState(false);
     const [userType, setUserType] = useState();
 
@@ -54,36 +54,117 @@ function Routes() {
             <>
                 {/* <Header isLogin={isLogin}/> */}
                 <Switch>
-                        <Route exact path="/"><MainPage isLogin={isLogin} /></Route>
-                        <Route exact path="/mypage"><Overview isLogin={isLogin} userType={userType}/></Route>
-                        <Route path="/mypage/notification"><Notification isLogin={isLogin} /></Route>
-                        <Route path="/mypage/requested-project"><RequestedProject isLogin={isLogin}/></Route>
-                        <Route path="/mypage/project"><MyProject isLogin={isLogin}/></Route>
-                        <Route path="/mypage/create-project"><CreateProject isLogin={isLogin}/></Route>
-                        <Route path="/mypage/requested-message"><RequestedMessage isLogin={isLogin} userType={userType}/></Route>
-                        <Route path="/mypage/bookmark"><BookMark isLogin={isLogin} userType={userType}/></Route>
+                    <Route exact path="/">
+                        <MainPage isLogin={isLogin} />
+                    </Route>
+                    <Route exact path="/mypage">
+                        <Overview isLogin={isLogin} userType={userType} />
+                    </Route>
+                    <Route path="/mypage/notification">
+                        <Notification isLogin={isLogin} />
+                    </Route>
+                    <Route path="/mypage/requested-project">
+                        <RequestedProject isLogin={isLogin} />
+                    </Route>
+                    <Route path="/mypage/project">
+                        <MyProject isLogin={isLogin} />
+                    </Route>
+                    <Route path="/mypage/create-project">
+                        <CreateProject isLogin={isLogin} />
+                    </Route>
+                    <Route path="/mypage/requested-message">
+                        <RequestedMessage
+                            isLogin={isLogin}
+                            userType={userType}
+                        />
+                    </Route>
+                    <Route path="/mypage/blocked-user">
+                        <BlockedUser isLogin={isLogin} userType={userType} />
+                    </Route>
+                    <Route path="/mypage/bookmark">
+                        <BookMark isLogin={isLogin} userType={userType} />
+                    </Route>
 
-                        <Route path="/login"><Login setIsLogin={setIsLogin} isLogin={isLogin} setUserType={setUserType}/></Route>
-                        <Route path="/logout"><Logout setIsLogin={setIsLogin} setUserType={setUserType}/></Route>
-                        <Route path="/signup"><Signup /></Route>
+                    <Route path="/login">
+                        <Login
+                            setIsLogin={setIsLogin}
+                            isLogin={isLogin}
+                            setUserType={setUserType}
+                        />
+                    </Route>
+                    <Route path="/logout">
+                        <Logout
+                            setIsLogin={setIsLogin}
+                            setUserType={setUserType}
+                        />
+                    </Route>
+                    <Route path="/signup">
+                        <Signup />
+                    </Route>
 
-                        <Route path="/whiteboard/:TcTnum"><WhiteBoard /></Route>
-                        <Route path="/TctWorkflow/:TcTnum"><TctWorkflow /></Route>
-                        <Route path="/TctModel/:skip/:TcTnum"><TctModel /></Route>
-                        <Route path="/TctPhotographer/:skip/:TcTnum"><TctPhotographer /></Route>
+                    <Route path="/whiteboard/:TcTnum">
+                        <WhiteBoard />
+                    </Route>
+                    <Route path="/TctWorkflow/:TcTnum">
+                        <TctWorkflow />
+                    </Route>
+                    <Route path="/TctModel/:skip/:TcTnum">
+                        <TctModel />
+                    </Route>
+                    <Route path="/TctPhotographer/:skip/:TcTnum">
+                        <TctPhotographer />
+                    </Route>
 
-                        <Route path="/model/Model/:skip/:sort"><Model> <Header isLogin={isLogin}/> </Model></Route>
-                        <Route path="/model/Model_Detail/:modelId"><Model_Detail> <Header isLogin={isLogin}/> </Model_Detail></Route>
-                        <Route path="/model/New_Model"><New_Model> <Header isLogin={isLogin} /> </New_Model></Route>
+                    <Route path="/model/Model/:skip/:sort">
+                        <Model>
+                            {" "}
+                            <Header isLogin={isLogin} />{" "}
+                        </Model>
+                    </Route>
+                    <Route path="/model/Model_Detail/:modelId">
+                        <Model_Detail>
+                            {" "}
+                            <Header isLogin={isLogin} />{" "}
+                        </Model_Detail>
+                    </Route>
+                    <Route path="/model/New_Model">
+                        <New_Model>
+                            {" "}
+                            <Header isLogin={isLogin} />{" "}
+                        </New_Model>
+                    </Route>
 
-                        <Route path="/Photographer/Photographer/:skip/:sort"><Photographer> <Header isLogin={isLogin}/> </Photographer></Route>
-                        <Route path="/Photographer/Photographer_Detail/:photographerId"><Photographer_Detail> <Header isLogin={isLogin}/> </Photographer_Detail></Route>
-                        <Route path="/Photographer/New_Photographer"><New_Photographer> <Header isLogin={isLogin} /> </New_Photographer></Route>
-                        <Route path="/notification/notification_Detail/:notificationNum"><NotificationDetail isLogin={isLogin} /></Route>
+                    <Route path="/Photographer/Photographer/:skip/:sort">
+                        <Photographer>
+                            {" "}
+                            <Header isLogin={isLogin} />{" "}
+                        </Photographer>
+                    </Route>
+                    <Route path="/Photographer/Photographer_Detail/:photographerId">
+                        <Photographer_Detail>
+                            {" "}
+                            <Header isLogin={isLogin} />{" "}
+                        </Photographer_Detail>
+                    </Route>
+                    <Route path="/Photographer/New_Photographer">
+                        <New_Photographer>
+                            {" "}
+                            <Header isLogin={isLogin} />{" "}
+                        </New_Photographer>
+                    </Route>
+                    <Route path="/notification/notification_Detail/:notificationNum">
+                        <NotificationDetail isLogin={isLogin} />
+                    </Route>
 
-                        <Route path="/collaboration/project/:currentPage/:sort"><Collaboration isLogin={isLogin}/></Route>
-                        <Route path="/collaboration/Create_Project"><NewCollaborate isLogin={isLogin}/></Route>
-                        <Route path="/collaboration/CollaborateDetail/:projectId"><CollaborateDetail isLogin={isLogin}/></Route>
+                    <Route path="/collaboration/project/:currentPage/:sort">
+                        <Collaboration isLogin={isLogin} />
+                    </Route>
+                    <Route path="/collaboration/Create_Project">
+                        <NewCollaborate isLogin={isLogin} />
+                    </Route>
+                    <Route path="/collaboration/CollaborateDetail/:projectId">
+                        <CollaborateDetail isLogin={isLogin} />
+                    </Route>
                 </Switch>
             </>
         </BrowserRouter>
