d6ad20c63fa0d2b782f0294b888940c59f6d3c23
Jiseon-Lee <l990831@naver.com>
2021-04-29 13:00:39 +0900

    main css

--- src/components/common/header.js
+++ src/components/common/header.js
@@ -10,13 +10,19 @@ function Header({ isLogin }) {
         <header className="main_header">
             <a href="/" className="logo">TWSM</a>
             <nav className="header_nav">
-                <ul>
-                    <li><Link to="/model/Model/0/L">model</Link></li>
-                    <li><Link to="/photographer/Photographer/0/L">photographer</Link></li>
-                    <li><Link to="/collaboration/project/1/L">collaborate</Link></li>
-                    <li>{isLogin ? <Link to="/mypage">my page</Link> : <Link to="/login">my page</Link>}</li>
-                    <li>{isLogin ? <Link to="/logout">logout</Link> : <Link to="/login">login</Link>}</li>
-                </ul>
+                <div className="header_nav_left">
+                    <ul>
+                        <li><Link to="/model/Model/0/L">model</Link></li>
+                        <li><Link to="/photographer/Photographer/0/L">photographer</Link></li>
+                        <li><Link to="/collaboration/project/1/L">collaborate</Link></li>
+                    </ul>
+                </div>
+                <div className="header_nav_right">
+                    <ul>
+                        <li>{isLogin ? <Link to="/mypage">my page</Link> : <Link to="/login">my page</Link>}</li>
+                        <li>{isLogin ? <Link to="/logout">logout</Link> : <Link to="/login">login</Link>}</li>
+                    </ul>
+                </div>
             </nav>
         </header>
     )
--- src/components/main/MainPage.js
+++ src/components/main/MainPage.js
@@ -1,14 +1,17 @@
 import React, { useEffect } from "react";
 import Header from "../common/header";
 import Search from "../common/search";
+import "./main.scss"
 
 function MainPage({ isLogin }) {
     console.log(isLogin);
     return(
         <div>
             <Header isLogin={isLogin}/>
-            <Search />
-            <p>main page</p>
+            <div className="main_contents">
+              <img src="../common/mainImage.png" alt="main_image" />
+              <button> Get started </button>
+            </div>
         </div>
     )
 }
