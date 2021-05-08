880adbe1dca9eccde9c61a2c239b274317b0d905
Jiseon-Lee <l990831@naver.com>
2021-05-09 00:18:30 +0900

    main page css

--- src/components/main/MainPage.js
+++ src/components/main/MainPage.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
 import Header from "../common/header";
 import mainImage from "./mainImage1.png";
 import "./main.scss"
@@ -6,13 +7,25 @@ import "./main.scss"
 function MainPage({ isLogin }) {
     console.log(isLogin);
     return(
-        <div>
+        <>
             <Header isLogin={isLogin}/>
             <div className="main_contents">
-              <img src={mainImage} alt="main_image" />
-              <button className="btn"> Get started </button>
+              <div className="image">
+              </div>
+              <div className="text_content">
+                <div className="title">
+                  <h1>THE WHOLE <br/>SHOOTING MATCH</h1>
+                </div>
+                <div className="explain">
+                  <p>Choose your model & photographer
+                    <br/>and get your team on the same page inside online meeting rooms with
+                    <br/>collaborative productivity tools!
+                  </p>
+                </div>
+                <Link className="btn" to="/model/Model/0/L"> Get started </Link>
+              </div>
             </div>
-        </div>
+        </>
     )
 }
 
