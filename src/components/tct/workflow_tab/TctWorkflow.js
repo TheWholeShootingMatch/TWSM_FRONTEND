24ca50e42984a314de6c6be8895e68f86eb1f547
Sa Eun Soo <tkdmstn0304@naver.com>
2021-05-09 03:51:51 +0900

    tct workflow merge

--- src/components/tct/workflow_tab/TctWorkflow.js
+++ src/components/tct/workflow_tab/TctWorkflow.js
@@ -1,228 +1,253 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { useFetch } from "../../common/useFetch"
+import { useFetch } from "../../common/useFetch";
 import { useHistory, useLocation, useParams } from "react-router-dom";
 
 import TctComponant from "../tct_componant/TctComponant";
 
-import Accordion from '@material-ui/core/Accordion';
-import AccordionSummary from '@material-ui/core/AccordionSummary';
-import AccordionDetails from '@material-ui/core/AccordionDetails';
-import Modal from '@material-ui/core/Modal';
+import Accordion from "@material-ui/core/Accordion";
+import AccordionSummary from "@material-ui/core/AccordionSummary";
+import AccordionDetails from "@material-ui/core/AccordionDetails";
+import Modal from "@material-ui/core/Modal";
 
 function Category() {
-  const [categorylist, setCategorylist] = useFetch('/api/category');
-  return (
-    <>
-      {categorylist.map((element, index) =>
-        <option value={element._id} key={index}>{element.name}</option>
-      )}
-    </>
-  );
+    const [categorylist, setCategorylist] = useFetch("/api/category");
+    return (
+        <>
+            {categorylist.map((element, index) => (
+                <option value={element._id} key={index}>
+                    {element.name}
+                </option>
+            ))}
+        </>
+    );
 }
 
 //modal
-function AddBtn({TcTnum}) {
+function AddBtn({ TcTnum }) {
+    // for modal
+    const [open, setOpen] = useState(false);
 
-  // for modal
-  const [open, setOpen] = useState(false);
+    const handleOpen = () => {
+        setOpen(true);
+    };
 
-  const handleOpen = () => {
-    setOpen(true);
-  };
+    const handleClose = () => {
+        setOpen(false);
+    };
 
-  const handleClose = () => {
-    setOpen(false);
-  };
+    // for post
+    const [inputs, setInputs] = useState({ TcTnum: TcTnum });
 
-  // for post
-  const [inputs, setInputs] = useState({TcTnum:TcTnum});
+    const handleChange = e => {
+        const { value, name } = e.target;
+        setInputs({
+            ...inputs,
+            [name]: value
+        });
+    };
 
-  const handleChange = (e) => {
-    const { value, name } = e.target;
-    setInputs({
-      ...inputs,
-      [name]: value
-    });
-  };
+    const handleSubmit = e => {
+        e.preventDefault();
+        axios
+            .post("/api/note", inputs)
+            .then(res => {
+                window.location.reload(true);
+            })
+            .catch(err => {
+                console.error(err);
+            });
+    };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    axios
-      .post('/api/note', inputs)
-      .then (res => { window.location.reload(true); })
-      .catch(err => { console.error(err); });
-  };
+    return (
+        <>
+            <button onClick={handleOpen}>add</button>
+            <Modal open={open} onClose={handleClose}>
+                <form className="new_note" onSubmit={handleSubmit}>
+                    <div className="new_header">
+                        <input
+                            type="text"
+                            name="title"
+                            placeholder="NoTitle"
+                            onChange={handleChange}
+                        />
+                        <select name="category" onChange={handleChange}>
+                            <option value={""}>select</option>
+                            <Category />
+                        </select>
+                    </div>
 
-  return (
-    <>
-      <button onClick={handleOpen}>add</button>
-      <Modal open={open} onClose={handleClose}>
-        <form className="new_note" onSubmit={handleSubmit}>
-          <div className="new_header">
-            <input type="text" name="title" placeholder="NoTitle" onChange={handleChange}/>
-            <select name="category" onChange={handleChange}>
-              <option value={""}>select</option>
-              <Category />
-            </select>
-          </div>
-
-          <input type="text" name="text" placeholder="Leave a message" onChange={handleChange}/>
-          <button type="submit">save</button>
-        </form>
-      </Modal>
-    </>
-  );
+                    <input
+                        type="text"
+                        name="text"
+                        placeholder="Leave a message"
+                        onChange={handleChange}
+                    />
+                    <button type="submit">save</button>
+                </form>
+            </Modal>
+        </>
+    );
 }
 
 // contents
 function Comment(props) {
-  return (
-    <div className="comment" id = {props._id}>
-      <p className="comment_name">{props.writer}</p>
-      <p className="comment_text">{props.contents}</p>
-    </div>
-  );
+    return (
+        <div className="comment" id={props._id}>
+            <p className="comment_name">{props.writer}</p>
+            <p className="comment_text">{props.contents}</p>
+        </div>
+    );
 }
 
 function NoteArea(props) {
-  //fetch comment
-  const param = {
-    method: "POST",
-    headers: {
-            'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ Lnum : props._id })
-  }
-  const [commentlist, setCommentlist] = useFetch(`/api/comment/fetch`, param);
+    //fetch comment
+    const param = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ Lnum: props._id })
+    };
+    const [commentlist, setCommentlist] = useFetch(`/api/comment/fetch`, param);
 
-  //post comment
-  const [inputs, setInputs] = useState({ Lnum:props._id, contents:"" });
+    //post comment
+    const [inputs, setInputs] = useState({ Lnum: props._id, contents: "" });
 
-  const handleChange = (e) => {
-    const { value, name } = e.target;
-    setInputs({
-      ...inputs,
-      [name]: value
-    });
-  };
+    const handleChange = e => {
+        const { value, name } = e.target;
+        setInputs({
+            ...inputs,
+            [name]: value
+        });
+    };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    axios
-      .post('/api/comment', inputs)
-      .then (res => { window.location.reload(true); })
-      .catch(err => { console.error(err); });
-  };
+    const handleSubmit = e => {
+        e.preventDefault();
+        axios
+            .post("/api/comment", inputs)
+            .then(res => {
+                window.location.reload(true);
+            })
+            .catch(err => {
+                console.error(err);
+            });
+    };
 
-  return (
-    <div className="note_area" id = {props._id}>
-      <div className="note_dot"></div>
+    return (
+        <div className="note_area" id={props._id}>
+            <div className="note_dot"></div>
 
-      <Accordion>
-        <AccordionSummary>
-          <div className="note_title">
-            <p className="category_tag">{props.category.name}</p>
-            <h3>{props.title}</h3>
-            <p>{props.contents}</p>
-          </div>
+            <Accordion>
+                <AccordionSummary>
+                    <div className="note_title">
+                        <p className="category_tag">{props.category.name}</p>
+                        <h3>{props.title}</h3>
+                        <p>{props.contents}</p>
+                    </div>
 
-          <div className="note_detail">
-            <p>{props.writer.id}</p>
-            <p>{props.logdate}</p>
-            <p>댓글 {commentlist.length}개</p>
-          </div>
-        </AccordionSummary>
-        <AccordionDetails>
-          {commentlist.map(({_id, writer, contents}, index) => <Comment
-            key={index}
-            _id = {_id}
-            writer = {writer.name}
-            contents = {contents}
-          />)}
-          <form onSubmit={handleSubmit}>
-            <input type="text" name="contents" onChange={handleChange} />
-            <button type="submit">send</button>
-          </form>
-        </AccordionDetails>
-      </Accordion>
-    </div>
-  );
+                    <div className="note_detail">
+                        <p>{props.writer.id}</p>
+                        <p>{props.logdate}</p>
+                        <p>댓글 {commentlist.length}개</p>
+                    </div>
+                </AccordionSummary>
+                <AccordionDetails>
+                    {commentlist.map(({ _id, writer, contents }, index) => (
+                        <Comment
+                            key={index}
+                            _id={_id}
+                            writer={writer.name}
+                            contents={contents}
+                        />
+                    ))}
+                    <form onSubmit={handleSubmit}>
+                        <input
+                            type="text"
+                            name="contents"
+                            onChange={handleChange}
+                        />
+                        <button type="submit">send</button>
+                    </form>
+                </AccordionDetails>
+            </Accordion>
+        </div>
+    );
 }
 
 function Contents() {
-  const { TcTnum } = useParams();
-  let history = useHistory();
-  let location = useLocation();
-  const find = new URLSearchParams(location.search);
+    const { TcTnum } = useParams();
+    let history = useHistory();
+    let location = useLocation();
+    const find = new URLSearchParams(location.search);
 
-  const findInput = {};
-  findInput.TcTnum = TcTnum;
-  if (find.get("category") !== null && find.get("category") !== "") {
-    findInput.category = find.get("category");
-  }
+    const findInput = {};
+    findInput.TcTnum = TcTnum;
+    if (find.get("category") !== null && find.get("category") !== "") {
+        findInput.category = find.get("category");
+    }
 
-  const [noteList, setNoteList] = useState([]);
+    const [noteList, setNoteList] = useState([]);
 
-  async function fetchUrl() {
-    const response = await fetch("/api/note/fetch",
-    {
-      method: "POST",
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({find : findInput})
-    });
+    async function fetchUrl() {
+        const response = await fetch("/api/note/fetch", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ find: findInput })
+        });
 
-    const json = await response.json();
-    setNoteList(json);
-  }
+        const json = await response.json();
+        setNoteList(json);
+    }
 
-  useEffect(() => {
-    fetchUrl();
-  }, [useLocation()]);
+    useEffect(() => {
+        fetchUrl();
+    }, [useLocation()]);
 
-  // category filter
-  const handleChange = (e) => {
-    e.preventDefault();
-    find.set(e.target.name, e.target.value);
+    // category filter
+    const handleChange = e => {
+        e.preventDefault();
+        find.set(e.target.name, e.target.value);
 
-    history.push(`/TctWorkflow/${TcTnum}?${find}`);
-  };
+        history.push(`/TctWorkflow/${TcTnum}?${find}`);
+    };
 
-  return (
-    <div className="tct_contents">
+    return (
+        <div className="tct_contents">
+            <div className="category_area">
+                <select name="category" onChange={handleChange}>
+                    <option value={""}>select</option>
+                    <Category />
+                </select>
+                <AddBtn TcTnum={TcTnum} />
+            </div>
 
-      <div className="category_area">
-        <select name="category" onChange={handleChange}>
-          <option value={""}>select</option>
-          <Category />
-        </select>
-        <AddBtn TcTnum={TcTnum}/>
-      </div>
-
-      <div className="note_list">
-        {noteList.map((elem, index) => <NoteArea
-          key={index}
-          _id = {elem._id}
-          writer = {elem.writer}
-          category = {elem.category}
-          title = {elem.title}
-          contents = {elem.contents}
-          logdate = {elem.logdate}
-        />)}
-      </div>
-    </div>
-  );
+            <div className="note_list">
+                {noteList.map((elem, index) => (
+                    <NoteArea
+                        key={index}
+                        _id={elem._id}
+                        writer={elem.writer}
+                        category={elem.category}
+                        title={elem.title}
+                        contents={elem.contents}
+                        logdate={elem.logdate}
+                    />
+                ))}
+            </div>
+        </div>
+    );
 }
 
 // wrap
 function TctWorkflow() {
-  return (
-    <TctComponant>
-      <Contents />
-    </TctComponant>
-  );
+    return (
+        <TctComponant linkType={true}>
+            <Contents />
+        </TctComponant>
+    );
 }
 
 export default TctWorkflow;
