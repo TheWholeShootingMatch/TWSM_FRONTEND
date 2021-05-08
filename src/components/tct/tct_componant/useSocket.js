22ef0f358ab65e3f11acf87bca700c0866420845
Sa Eun Soo <tkdmstn0304@naver.com>
2021-05-09 03:50:31 +0900

    [error fixed] send tct num when click image

--- src/components/tct/tct_componant/useSocket.js
+++ src/components/tct/tct_componant/useSocket.js
@@ -5,54 +5,61 @@ import axios from "axios";
 const NEW_SELECT_EVENT = "newSelect";
 const SOCKET_SERVER_URL = ":3001";
 
-const useSocket = (roomId) => {
-  console.log("roomId",roomId);
-  const [selectedList, setSelectedList] = useState([]);
-  const socketRef = useRef();
+const useSocket = roomId => {
+    console.log("roomId", roomId);
+    const [selectedList, setSelectedList] = useState([]);
+    const socketRef = useRef();
 
-  useEffect(() => {
-    socketRef.current = socketIOClient(SOCKET_SERVER_URL, {
-      query: { roomId },
-    });
+    useEffect(() => {
+        socketRef.current = socketIOClient(SOCKET_SERVER_URL, {
+            query: { roomId }
+        });
 
-    socketRef.current.on(NEW_SELECT_EVENT, (selectedList) => {
-      const incoming = {
-        ...selectedList
-      };
+        socketRef.current.on(NEW_SELECT_EVENT, selectedList => {
+            const incoming = {
+                ...selectedList
+            };
 
-      const exist = (arr) => {
-        let bool = false;
+            const exist = arr => {
+                let bool = false;
 
-        arr.map((elem) => {
-          if (elem.body === incoming.body) {
-            bool = true;
-        }});
+                arr.map(elem => {
+                    if (elem.body === incoming.body) {
+                        bool = true;
+                    }
+                });
 
-        return bool;
-      }
+                return bool;
+            };
 
-      if (incoming.func === "P") {
-        setSelectedList((selectedList) => exist(selectedList) ? [...selectedList] : [...selectedList, incoming]);
-      }
-      else {
-        setSelectedList((selectedList) => selectedList.filter(elem => elem.body !== incoming.body));
-      }
-    });
+            if (incoming.func === "P") {
+                setSelectedList(selectedList =>
+                    exist(selectedList)
+                        ? [...selectedList]
+                        : [...selectedList, incoming]
+                );
+            } else {
+                setSelectedList(selectedList =>
+                    selectedList.filter(elem => elem.body !== incoming.body)
+                );
+            }
+        });
 
-    return () => {
-      socketRef.current.disconnect();
+        return () => {
+            socketRef.current.disconnect();
+        };
+    }, [roomId]);
+
+    const sendSelectedList = input => {
+        socketRef.current.emit(NEW_SELECT_EVENT, {
+            body: input.id,
+            func: input.func,
+            type: input.type,
+            TcTnum: input.TcTnum
+        });
     };
-  }, [roomId]);
 
-  const sendSelectedList = (input) => {
-    socketRef.current.emit(NEW_SELECT_EVENT, {
-      body: input.id,
-      func: input.func,
-      type: input.type
-    });
-  };
-
-  return { selectedList, sendSelectedList };
+    return { selectedList, sendSelectedList };
 };
 
 export default useSocket;
